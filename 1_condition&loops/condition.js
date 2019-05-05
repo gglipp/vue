5e0bb2abe223e0a69cf836843e81b3c0c60d04d6
panlijun <panlijun@vip.126.com>
2019-05-05 22:10:40 +0800

    condition&loops

---
+++ 1_condition&loops/condition.js
@@ -0,0 +1,17 @@
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'study js' },
+            { text: 'study Vue' },
+            { text: 'study English' }
+        ]
+    }
+})
