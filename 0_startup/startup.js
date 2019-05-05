16d3f2a5fe7095d30f956c38d1196ac1cb4ce2f0
panlijun <panlijun@vip.126.com>
2019-05-05 21:53:51 +0800

    startup

--- 0_startup/startup.js
+++ 0_startup/startup.js
@@ -6,3 +6,11 @@ var app = new Vue({
 })
 
 app.message = 'message changed!'
+
+
+var app2 = new Vue({
+    el : '#app-2',
+    data : {
+        message : '页面加载于 ' + new Date().toLocaleString()
+    }
+})
