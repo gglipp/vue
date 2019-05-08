668d86fe2786a06e0c859c8180aa2f5a9af32623
panlijun <panlijun@vip.126.com>
2019-05-08 15:40:21 +0800

    calc

--- 6_calcProp&listener/calc.js
+++ 6_calcProp&listener/calc.js
@@ -1,11 +1,37 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'this is origin message!'
+        message: 'this is origin message!',
+        data: 'this is data',
+        myName: 'lijun pan',
+        firstName: '',
+        lastName: ''
     },
     computed: {
         reversedMessage: function() {
             return this.message.split('').reverse().join('')
+        },
+        now: function() {
+            return Date.now()
+        },
+        name: {
+            get: function() {
+                return this.myName
+            },
+            set: function(valName) {
+                console.info(valName),
+                this.myName= valName,
+                names = valName.split(' '),
+                console.info('names ' + names),
+                this.firstName = names[0],
+                this.lastName = names[1]
+            }
+        }
+    },
+    watch: {
+        message: function(val) {
+            console.info(val),
+            this.data= val + '+'
         }
     },
     
