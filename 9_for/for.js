72a8cf4ac8b259d5e8d517625e61661224c56194
gglipp <gglipp@126.com>
2019-05-09 17:46:47 +0800

    v-for

---
+++ 9_for/for.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { text: 'go for reading' },
+            {
+                text: 'go for lunch'
+            },
+            {
+                text: 'do somothing else'
+            }
+        ]
+    }
+})
