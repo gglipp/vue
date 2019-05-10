bdee7399bb925b8f70721e094c4aabda3fe1be53
gglipp <gglipp@126.com>
2019-05-10 17:58:32 +0800

    form

---
+++ 11_form/form.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        input_message: 'this is input message',
+        text_message: 'this is text message',
+        checkbox: true,
+        names:[]
+    }
+})
